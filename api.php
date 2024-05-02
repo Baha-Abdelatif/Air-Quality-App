b6001da12f42f39316cc130da5c317007de1e964
Baha-Abdelatif <baha.abdelatif@gmail.com>
2024-05-02 20:49:06 +0200

    Fetching data from OpenAQ API.

--- api.php
+++ api.php
@@ -3,7 +3,7 @@ require_once 'vendor/autoload.php';
 
 $client = new \GuzzleHttp\Client(['verify' => false]);
 
-$response = $client->request('GET', 'https://api.openaq.org/v2/latest?page=1&offset=0&sort=desc&radius=1000&order_by=lastUpdated&dump_raw=false', [
+$response = $client->request('GET', 'https://api.openaq.org/v2/locations?country=fr', [
   'headers' => [
     'accept' => 'application/json',
     'content-type' => 'application/json',
