65bc845788f68540ff0ff880a881d884f3e39ad3
Baha-Abdelatif <baha.abdelatif@gmail.com>
2024-05-02 22:31:35 +0200

    Rebasing folder structure

--- api.php
+++ api.php
@@ -1,13 +1,13 @@
 <?php
-require_once 'vendor/autoload.php';
+// require_once 'vendor/autoload.php';
 
-$client = new \GuzzleHttp\Client(['verify' => false]);
+// $client = new \GuzzleHttp\Client(['verify' => false]);
 
-$response = $client->request('GET', 'https://api.openaq.org/v2/locations?country=fr', [
-  'headers' => [
-    'accept' => 'application/json',
-    'content-type' => 'application/json',
-  ],
-]);
+// $response = $client->request('GET', 'https://api.openaq.org/v2/locations?country=fr', [
+//   'headers' => [
+//     'accept' => 'application/json',
+//     'content-type' => 'application/json',
+//   ],
+// ]);
 
-echo $response->getBody();
+// echo $response->getBody();
--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
-require_once __DIR__ . "/header.html";
+require_once __DIR__ . "/views/header.html";
+// $data = json_decode(
+//   file_get_contents('compress.bzip2://' . __DIR__ . "/data/cancun.json.bz2"),
+//   true,
+// );
 ?>
 <main>
-
   <?php
-  require_once __DIR__ . "/api.php";
+  // var_dump($data);
+
 
   ?>
 </main>
 <?php
-require_once __DIR__ . "/footer.html";
+require_once __DIR__ . "/views/footer.html";
 ?>
